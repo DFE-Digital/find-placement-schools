657b125d5c83ba5c8a391be984a896c156ac9a08
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-21 16:18:04 +0100

    Mount mission control

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
