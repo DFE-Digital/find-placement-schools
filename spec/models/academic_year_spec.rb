747c62eed7da7d284b689d6884c3553a174442c9
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-21 16:18:51 +0100

    Add `for_date` and `current` methods to academic year

--- spec/models/academic_year_spec.rb
+++ spec/models/academic_year_spec.rb
@@ -18,4 +18,56 @@ RSpec.describe AcademicYear, type: :model do
     it { is_expected.to validate_presence_of(:ends_on) }
     it { is_expected.to validate_comparison_of(:ends_on).is_greater_than_or_equal_to(:starts_on) }
   end
+
+  describe ".for_date" do
+    let!(:existing_academic_year) do
+      create(:academic_year,
+             name: "2022 to 2023",
+             starts_on: Date.parse("1 September 2022"),
+             ends_on: Date.parse("31 August 2023"))
+    end
+
+    context "when date is within an existing academic year" do
+      it "returns the existing academic year" do
+        date = Date.parse("7 November 2022")
+        expect(described_class.for_date(date)).to eq(existing_academic_year)
+      end
+    end
+
+    context "when date is not within an existing academic year" do
+      it "creates a new academic year" do
+        date = Date.parse("5 October 2026")
+        expect { described_class.for_date(date) }.to change(described_class, :count).by(1)
+        new_academic_year = described_class.for_date(date)
+        expect(new_academic_year.starts_on).to eq(Date.parse("1 September 2026"))
+        expect(new_academic_year.ends_on).to eq(Date.parse("31 August 2027"))
+        expect(new_academic_year.name).to eq("2026 to 2027")
+      end
+    end
+
+    context "when start month is before September and academic year does not exist" do
+      it "creates a new academic year starting the previous year" do
+        date = Date.parse("14 March 2026")
+        new_academic_year = described_class.for_date(date)
+        expect(new_academic_year.starts_on).to eq(Date.parse("1 September 2025"))
+        expect(new_academic_year.ends_on).to eq(Date.parse("31 August 2026"))
+        expect(new_academic_year.name).to eq("2025 to 2026")
+      end
+    end
+  end
+
+  describe ".current" do
+    let!(:academic_year) do
+      create(:academic_year,
+             starts_on: Date.parse("1 September 2020"),
+             ends_on: Date.parse("31 August 2021"),
+             name: "2024 to 2025")
+    end
+
+    it "returns the academic year for the current date" do
+      Timecop.travel(Date.parse("5 December 2020")) do
+        expect(described_class.current).to eq(academic_year)
+      end
+    end
+  end
 end
