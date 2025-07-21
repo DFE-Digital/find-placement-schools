e446089a36bfc3a2d64728a1e1a813209d59fd46
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-21 16:20:27 +0100

    Add GIAS import

--- spec/factories/placement_preferences.rb
+++ spec/factories/placement_preferences.rb
@@ -1,4 +1,18 @@
 FactoryBot.define do
   factory :placement_preference do
+    trait :actively_looking do
+      appetite { "actively_looking" }
+    end
+
+    trait :interested do
+      appetite { "interested" }
+    end
+
+    trait :not_open do
+      appetite { "not_open" }
+    end
+
+    association :academic_year, :current
+    association :created_by, factory: :user
   end
 end
---
+++ spec/services/gias/csv_downloader_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe GIAS::CSVDownloader do
+  subject(:gias_csv_downloader) { described_class.call }
+
+  let(:file) { Tempfile.new }
+
+  after { file.unlink }
+
+  it_behaves_like "a service object"
+
+  it "downloads the GIAS CSV for yesterday's date" do
+    yesterday = Time.zone.yesterday.strftime("%Y%m%d")
+    gias_filename = "edubasealldata#{yesterday}.csv"
+
+    allow(Down).to receive(:download).with(
+      "#{ENV["GIAS_CSV_BASE_URL"]}/#{gias_filename}",
+    ).and_return(file)
+
+    expect(gias_csv_downloader).to eq(file)
+  end
+
+  it "sets the file encoding to ISO-8859-1" do
+    allow(Down).to receive(:download).and_return(file)
+    downloaded = gias_csv_downloader
+    expect(downloaded.external_encoding).to be(Encoding::ISO_8859_1)
+    expect(downloaded.internal_encoding).to be(Encoding::UTF_8)
+  end
+end
