1d732e15eb46715f6b9c93a9c5c6d68920583925
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-21 16:20:01 +0100

    Delegate organisation address fields for geocoding

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -6,4 +6,20 @@ class Organisation < ApplicationRecord
   has_many :users, through: :user_memberships
 
   validates :name, presence: true
+
+  delegate :address_1, :address_2, :address_3, :town, :postcode, to: :organisation_address, prefix: false, allow_nil: true
+
+  geocoded_by :address_string
+
+  private
+
+  def address_string
+    [
+     address_1,
+     address_2,
+     address_3,
+     town,
+     postcode
+    ].compact_blank.join(", ")
+  end
 end
