711b622a16d38da2e5fc960e77df0e59ff2dcc6b
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-24 11:24:18 +0100

    Update home page

---
+++ spec/system/pages/user_views_home_page_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "User views home page", type: :system do
+  scenario do
+    given_i_navigate_to_the_service
+    then_i_see_the_home_page
+  end
+
+  private
+
+  def given_i_navigate_to_the_service
+    visit root_path
+  end
+
+  def then_i_see_the_home_page
+    expect(page).to have_title("Find ITT placements")
+    expect(page).to have_h1("Find initial teacher training (ITT) placements")
+    expect(page).to have_link("Start now", href: "/sign-in")
+    expect(page).to have_paragraph("We can add some content here!")
+    expect(page).to have_h3("Related content")
+    expect(page).to have_paragraph("Coming soon")
+  end
+end
