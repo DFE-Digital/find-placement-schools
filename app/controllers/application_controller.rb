4379ef7d208939a36f53189ddf88a4aeb54fbba9
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-24 07:52:42 +0100

    Add pagy gem and pagination component

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
