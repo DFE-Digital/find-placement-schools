501f564a4d09d7fca228baa5b81b34c9f975f3ec
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-24 13:04:08 +0100

    Fix line indentation, surprised the linter didn't catch this!

--- app/controllers/concerns/organisation_redirectable.rb
+++ app/controllers/concerns/organisation_redirectable.rb
@@ -8,22 +8,21 @@ module OrganisationRedirectable
       return
     end
 
-  organisation =
-    if organisation_id
-      user.organisations.find_by(id: organisation_id)
+    organisation = if organisation_id
+        user.organisations.find_by(id: organisation_id)
     elsif user.organisations.one?
-      user.organisations.first
+        user.organisations.first
     end
 
-  if organisation.present?
-    session[:organisation_id] = organisation.id
-    notice = organisation_id ? "You have changed your organisation to #{organisation.name}" : "Signed in as #{user.first_name} #{user.last_name}"
+    if organisation.present?
+      session[:organisation_id] = organisation.id
+      notice = organisation_id ? "You have changed your organisation to #{organisation.name}" : "Signed in as #{user.first_name} #{user.last_name}"
 
-    path = organisation.is_a?(School) ? placement_preferences_path : organisations_path
-    redirect_to path, notice:
-  else
-    alert = organisation_id ? "You do not have access to this organisation." : nil
-    redirect_to change_organisation_index_path, alert:
-  end
+      path = organisation.is_a?(School) ? placement_preferences_path : organisations_path
+      redirect_to path, notice:
+    else
+      alert = organisation_id ? "You do not have access to this organisation." : nil
+      redirect_to change_organisation_index_path, alert:
+    end
   end
 end
