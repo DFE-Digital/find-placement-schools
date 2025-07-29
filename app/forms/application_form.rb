f3070bbe5357d886d65f98beb8b07748be3274a1
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-29 16:18:42 +0100

    Add application_form.rb

---
+++ app/forms/application_form.rb
@@ -0,0 +1,33 @@
+class ApplicationForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+  include ActionView::Helpers::TranslationHelper
+  include Rails.application.routes.url_helpers
+
+  def initialize(attributes = {})
+    @virtual_path = "forms/#{self.class.name.underscore}"
+
+    super
+  end
+
+  # Defaults to doing nothing and signals a successful save.
+  # Override this method in your subclass to implement your own persistence logic.
+  # Side effects like sending notifications should not be triggered from this method.
+  def persist
+    true
+  end
+
+  def save
+    validate ? persist : false
+  end
+
+  def save!
+    validate ? persist : raise_validation_error
+  end
+
+  private
+
+  def raise_validation_error
+    raise ActiveModel::ValidationError, self
+  end
+end
