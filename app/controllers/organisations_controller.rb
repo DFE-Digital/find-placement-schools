26208fa49b26284136862b53ea722a77911d3f4f
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-29 16:23:14 +0100

    Implement initial provider filters

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,4 +1,8 @@
 class OrganisationsController < ApplicationController
+  before_action :store_filter_params, only: %i[index]
+
+  helper_method :location_coordinates
+
   def show
     @organisation = Organisation.find(params[:id])
 
@@ -6,8 +10,55 @@ class OrganisationsController < ApplicationController
   end
 
   def index
-    @pagy, @schools = pagy(School.all)
+    schools = SchoolsQuery.call(params: query_params)
+    @pagy, @schools = pagy(schools)
+    @schools = @schools.decorate
+    calculate_travel_time
 
-    render locals: { pagy: @pagy, schools: @schools.decorate }
+    render locals: { pagy: @pagy, schools: @schools, filter_form: }
+  end
+
+  private
+
+  def filter_form
+    @filter_form = Organisations::FilterForm.new(filter_params)
+  end
+
+  def search_location
+    @search_location ||= params[:search_location] || params.dig(:filters, :search_location)
+  end
+
+  def location_coordinates
+    return if search_location.blank?
+
+    @location_coordinates ||= Geocoder::Search.call(search_location).coordinates
+  end
+
+  def filter_params
+    params.fetch(:filters, {}).permit(
+      :search_location,
+      :search_by_name,
+      phases: [],
+      )
+  end
+
+  def query_params
+    {
+      filters: filter_form.query_params,
+      location_coordinates: location_coordinates
+    }
+  end
+
+  def calculate_travel_time
+    return if search_location.blank?
+
+    TravelTime.call(
+      origin_address: search_location,
+      destinations: @schools.uniq,
+    )
+  end
+
+  def store_filter_params
+    session["find_filter_params"] = { filters: filter_params, page: params[:page] }
   end
 end
---
+++ app/forms/organisations/filter_form.rb
@@ -0,0 +1,70 @@
+class Organisations::FilterForm < ApplicationForm
+  include ActiveModel::Attributes
+
+  attribute :search_location, default: nil
+  attribute :search_by_name, default: nil
+  attribute :phases, default: []
+
+  def initialize(params = {})
+    params.each_value { |v| v.compact_blank! if v.is_a?(Array) }
+
+    super(params)
+  end
+
+  def filters_selected?
+    attributes
+      .values
+      .compact
+      .flatten
+      .select(&:present?)
+      .any?
+  end
+
+  def clear_filters_path
+    organisations_path
+  end
+
+  def index_path_without_filter(filter:, value: nil)
+    without_filter = if SINGULAR_ATTRIBUTES.include?(filter)
+                       compacted_attributes.reject { |key, _| key == filter }
+    else
+                       compacted_attributes.merge(filter => compacted_attributes[filter].reject { |filter_value| filter_value == value })
+    end
+
+    organisations_path(
+      params: { filters: without_filter },
+    )
+  end
+
+  def query_params
+    {
+      search_location:,
+      search_by_name:,
+      phases:
+    }
+  end
+
+  def primary_selected?
+    phases.include?("Primary")
+  end
+
+  def secondary_selected?
+    phases.include?("Secondary")
+  end
+
+  def primary_only?
+    primary_selected? && !secondary_selected?
+  end
+
+  def secondary_only?
+    !primary_selected? && secondary_selected?
+  end
+
+  private
+
+  SINGULAR_ATTRIBUTES = %w[search_location search_by_name].freeze
+
+  def compacted_attributes
+    @compacted_attributes ||= attributes.compact_blank
+  end
+end
