ffa8552fe81541bc03ec12092c5ed2f4c6653c52
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-29 16:22:59 +0100

    Create google routes API to allow the travel time to be calculated

---
+++ lib/google/routes_api.rb
@@ -0,0 +1,50 @@
+module Google
+  class RoutesApi
+    include HTTParty
+
+    UNITS = "IMPERIAL".freeze
+
+    base_uri "https://routes.googleapis.com"
+    headers "Accept" => "application/json",
+            "Content-Type" => "application/json;odata.metadata=minimal",
+            "X-Goog-Api-Key" => ENV.fetch("GOOGLE_MAPS_API_KEY", "").to_s,
+            "X-Goog-FieldMask" => "localizedValues,destinationIndex"
+
+    def travel_time(origin_address, destinations, travel_mode: "DRIVE")
+      response = self.class.post("/distanceMatrix/v2:computeRouteMatrix", body: options(origin_address, destinations, travel_mode))
+      JSON.parse(response.body)
+    end
+
+    private
+
+    def options(origin_address, destinations, travel_mode)
+      {
+        origins: [ transform_origin(origin_address) ],
+        destinations: destinations.map { |destination| transform_destination(destination) },
+        travelMode: travel_mode,
+        units: UNITS
+      }.to_json
+    end
+
+    def transform_destination(destination)
+      {
+        waypoint: {
+          location: {
+            latLng: {
+              latitude: destination.latitude,
+              longitude: destination.longitude
+            }
+          }
+        }
+      }
+    end
+
+    def transform_origin(address)
+      {
+        waypoint: {
+          address:
+        }
+      }
+    end
+  end
+end
