2c7a05ab005c10d22299e56dd70bdda4bfe375fe
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-07-29 16:21:49 +0100

    Add travel time attributes to school model

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,8 @@
 class School < Organisation
+  attribute :transit_travel_duration
+  attribute :walk_travel_duration
+  attribute :drive_travel_duration
+
   validates :urn, presence: true, uniqueness: true
 
   has_many :placement_preferences, foreign_key: :organisation_id
