808af54204d148dd7d3b616bcb8d93e5df3c5e4e
Jamie <jamiecleare2525@hotmail.com>
2025-08-01 13:32:56 +0100

    Unit tests and view fixes

--- spec/factories/placement_preferences.rb
+++ spec/factories/placement_preferences.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :placement_preference do
+    appetite { "actively_looking" }
+
     trait :actively_looking do
       appetite { "actively_looking" }
     end
@@ -13,6 +15,7 @@ FactoryBot.define do
     end
 
     association :academic_year, :current
+    association :organisation, factory: :school
     association :created_by, factory: :user
   end
 end
--- spec/models/placement_preference_spec.rb
+++ spec/models/placement_preference_spec.rb
@@ -20,4 +20,39 @@ RSpec.describe PlacementPreference, type: :model do
         .backed_by_column_of_type(:enum)
     end
   end
+
+
+  describe "scopes" do
+    describe "#for_academic_year" do
+      let(:academic_year_2324) do
+        build(:academic_year,
+             starts_on: Date.parse("1 September 2023"),
+             ends_on: Date.parse("31 August 2024"),
+             name: "2023 to 2024")
+      end
+      let!(:academic_year_2425) do
+        build(:academic_year,
+             starts_on: Date.parse("1 September 2024"),
+             ends_on: Date.parse("31 August 2025"),
+             name: "2024 to 2025")
+      end
+      let(:placement_preference_2324) do
+        create(:placement_preference, academic_year: academic_year_2324)
+      end
+      let!(:placement_preference_2425) do
+        create(:placement_preference, academic_year: academic_year_2425)
+      end
+
+      before do
+        academic_year_2324
+        placement_preference_2324
+      end
+
+      it "returns only the placement preferences for the given academic year" do
+        expect(
+          described_class.for_academic_year(academic_year_2425)
+        ).to contain_exactly(placement_preference_2425)
+      end
+    end
+  end
 end
