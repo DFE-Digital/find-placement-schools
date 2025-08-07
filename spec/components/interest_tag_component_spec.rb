06f74d45e1fe7df98599e1f4a7147100ab80f7bd
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-08-07 11:50:43 +0100

    Create interest tag component

---
+++ spec/components/interest_tag_component_spec.rb
@@ -0,0 +1,62 @@
+require "rails_helper"
+
+RSpec.describe InterestTagComponent, type: :component do
+  subject(:component) do
+    described_class.new(school:, academic_year:)
+  end
+
+  let(:academic_year) { AcademicYear.current }
+  let(:school) { create(:school, placement_preferences:) }
+
+  before do
+    render_inline(component)
+  end
+
+  context "when the school is actively looking" do
+    let(:placement_preferences) { [build(:placement_preference, appetite: "actively_looking")] }
+
+    it "renders the correct text" do
+      expect(page).to have_content "Placements available"
+    end
+
+    it "renders the correct tag" do
+      expect(page).to have_css(".govuk-tag--green")
+    end
+  end
+
+  context "when the school is interested in hosting" do
+    let(:placement_preferences) { [build(:placement_preference, appetite: "interested")] }
+
+    it "renders the correct text" do
+      expect(page).to have_content "May offer placements"
+    end
+
+    it "renders the correct tag" do
+      expect(page).to have_css(".govuk-tag--yellow")
+    end
+  end
+
+  context "when the school is not open to hosting" do
+    let(:placement_preferences) { [build(:placement_preference, appetite: "not_open")] }
+
+    it "renders the correct text" do
+      expect(page).to have_content "Not offering placements"
+    end
+
+    it "renders the correct tag" do
+      expect(page).to have_css(".govuk-tag--red")
+    end
+  end
+
+  context "when the school has not specified a placement preference" do
+    let(:school) { create(:school) }
+
+    it "renders the correct text" do
+      expect(page).to have_content "Placement availability unknown"
+    end
+
+    it "renders the correct tag" do
+      expect(page).to have_css(".govuk-tag--grey")
+    end
+  end
+end
