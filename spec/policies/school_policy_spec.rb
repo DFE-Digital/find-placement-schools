10d0b05bea1add6b957d4f6adacf8096a0abaa64
Jamie <jamiecleare2525@hotmail.com>
2025-08-19 14:11:38 +0100

    Lint

--- spec/policies/school_policy_spec.rb
+++ spec/policies/school_policy_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe SchoolPolicy do
   let(:school) { create(:school) }
   let(:user) { create(:user, organisations:, selected_organisation:) }
   let(:selected_organisation) { nil }
-  let(:organisations) { [school] }
+  let(:organisations) { [ school ] }
 
   permissions :show? do
     context "when the selected organisation is not the school record" do
       let(:another_school) { create(:school) }
-      let(:organisations) { [another_school] }
+      let(:organisations) { [ another_school ] }
       let(:selected_organisation) { another_school }
 
       it "denies access" do
@@ -30,7 +30,7 @@ RSpec.describe SchoolPolicy do
     context "when the selected organisation is a provider" do
       let(:provider) { create(:provider) }
       let(:selected_organisation) { provider }
-      let(:organisations) { [provider] }
+      let(:organisations) { [ provider ] }
 
       it "permits access" do
         expect(school_policy).to permit(user, school)
