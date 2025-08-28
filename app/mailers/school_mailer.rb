714377084027653680614cfb951abb5e0964c3eb
Jamie <jamiecleare2525@hotmail.com>
2025-08-28 16:51:25 +0100

    Add locales for mailer

--- app/mailers/school_mailer.rb
+++ app/mailers/school_mailer.rb
@@ -5,6 +5,6 @@ class SchoolMailer < ApplicationMailer
     @placement_details = school.placement_preference_for(academic_year: AcademicYear.next).placement_details
     email_address = @placement_details.dig("school_contact", "email_address")
 
-    notify_email to: email_address, subject: "Help hosting ITT placements"
+    notify_email to: email_address, subject: t(".help_hosting_itt_placements")
   end
 end
