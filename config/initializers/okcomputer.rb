bf05275fde0d1485db4c8556579407086436a4a2
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-09-26 16:32:03 +0100

    Fix hosts issue and add current env healthcheck

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,4 +1,12 @@
 OkComputer.mount_at = "healthcheck"
 
+class CurrentEnvCheck < OkComputer::Check
+  def check
+    mark_message ENV["RAILS_ENV"]
+    true
+  end
+end
+
 OkComputer::Registry.register "database", OkComputer::ActiveRecordCheck.new
 OkComputer::Registry.register "sha", OkComputer::AppVersionCheck.new
+OkComputer::Registry.register "current_env", CurrentEnvCheck.new
