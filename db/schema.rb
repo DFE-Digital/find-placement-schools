6599745b2787ea53473dae95c54bd99c43adce43
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-09-29 13:04:41 +0100

    Remove plpgsqp

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_29_104848) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
   # Custom types defined in this database.
