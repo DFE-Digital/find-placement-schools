0e7f590afd69df0098b79559693416e6792f6ad0
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-09-29 17:33:26 +0100

    Remove enable_extension plpgsql

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_09_25_093452) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
   create_enum "appetite", ["actively_looking", "interested", "not_open"]
