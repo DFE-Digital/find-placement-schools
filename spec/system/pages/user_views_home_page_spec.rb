c7b97073b8985da4bfda4f256c33a4dbfaeb45ed
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-10-01 11:02:38 +0100

    Fix failing spec and add az_development config

--- spec/system/pages/user_views_home_page_spec.rb
+++ spec/system/pages/user_views_home_page_spec.rb
@@ -14,10 +14,8 @@ RSpec.describe "User views home page", type: :system do
 
   def then_i_see_the_home_page
     expect(page).to have_title("Find placement schools")
-    expect(page).to have_h1("Find placement schools")
+    expect(page).to have_h2("Register placement schools")
+    expect(page).to have_h2("Find placement schools")
     expect(page).to have_link("Start now", href: "/sign-in")
-    expect(page).to have_paragraph("We can add some content here!")
-    expect(page).to have_h3("Related content")
-    expect(page).to have_paragraph("Coming soon")
   end
 end
