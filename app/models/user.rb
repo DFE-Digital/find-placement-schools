02e2f71f800cdb94a4d790cb61cc56e49730db7a
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-10-07 15:37:02 +0100

    Add full_name method to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, presence: true
   validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Please enter a valid email address" }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
