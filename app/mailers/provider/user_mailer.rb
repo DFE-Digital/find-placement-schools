4b20ff019aa3c7f1a23d7a5ce2a0dbb1e2d34964
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-10-07 14:48:54 +0100

    Add provider user invite emails to provider user mailer

---
+++ app/mailers/provider/user_mailer.rb
@@ -0,0 +1,54 @@
+class Provider::UserMailer < ApplicationMailer
+  def placement_preferences_notification(user)
+    @user = User.find(user.id)
+    notify_email to: @user.email_address,
+                 subject: t(".subject"),
+                 body: I18n.t(
+                 "school.user_mailer.placement_preferences_notification.body",
+                    user_name: @user.first_name,
+                    school_names: School
+                    .without_preference_for(AcademicYear.current)
+                    .joins(:user_memberships)
+                    .where(user_memberships: { user_id: user.id })
+                    .distinct
+                    .pluck(:name)
+                    .join(", ")
+                  )
+  end
+
+  def user_membership_created_notification(user, organisation)
+    notify_email to: user.email_address,
+                 subject: t(".subject", service_name:),
+                 body: t(
+                   ".body",
+                   user_name: user.first_name,
+                   organisation_name: organisation.name,
+                   service_name:,
+                   academic_year_name: AcademicYear.current.decorate.name,
+                   support_email:,
+                   sign_in_url: sign_in_url(utm_source: "email", utm_medium: "notification", utm_campaign: "school"),
+                 )
+  end
+
+  def user_membership_destroyed_notification(user, organisation)
+    notify_email to: user.email_address,
+                 subject: t(".subject", service_name:),
+                 body: t(
+                   ".body",
+                   user_name: user.first_name,
+                   organisation_name: organisation.name,
+                   service_name:,
+                   support_email:,
+                 )
+  end
+
+  private
+
+  def support_email
+    t("layouts.footer.support_email")
+  end
+
+  def service_name
+    t("service.name")
+  end
+end
---
+++ spec/mailers/provider/user_mailer_spec.rb
@@ -0,0 +1,99 @@
+require "rails_helper"
+
+RSpec.describe Provider::UserMailer, type: :mailer do
+  describe "#user_membership_created_notification" do
+    subject(:invite_email) { described_class.user_membership_created_notification(user, organisation) }
+
+    let(:user) { create(:user) }
+    let(:organisation) { create(:school) }
+
+    it "sends invitation email" do
+      expect(invite_email.to).to contain_exactly(user.email_address)
+      expect(invite_email.subject).to eq("[DEVELOPMENT] Invitation to join Find placement schools")
+      expect(invite_email.body.to_s.squish).to eq(<<~EMAIL.squish)
+        Dear #{user.first_name},
+
+        You have been invited to join the Find placement schools service for #{organisation.name}.
+
+        Use this service to find placement schools for your trainee teachers.
+
+        This service is being trialled by the Department for Education with schools and teacher training providers in England.
+
+        [Sign in to Find placement schools](http://localhost/sign-in?utm_campaign=school&utm_medium=notification&utm_source=email)
+
+        If you do not have DfE Sign-in, create an account. You can then return to this email to access the service.
+
+        If you need help or have feedback for us, contact [find.placementschools@education.gov.uk](mailto:find.placementschools@education.gov.uk).
+
+        Regards
+
+        Find placement schools team
+      EMAIL
+    end
+
+    context "when HostingEnvironment.env is 'production'" do
+      before do
+        allow(HostingEnvironment).to receive(:env).and_return("production")
+      end
+
+      it "does not prepend the hosting environment to the subject" do
+        expect(invite_email.subject).to eq("Invitation to join Find placement schools")
+      end
+    end
+
+    context "when HostingEnvironment.env is 'staging'" do
+      before do
+        allow(HostingEnvironment).to receive(:env).and_return("staging")
+      end
+
+      it "prepends the hosting environment to the subject" do
+        expect(invite_email.subject).to eq("[STAGING] Invitation to join Find placement schools")
+      end
+    end
+  end
+
+  describe "#user_membership_destroyed_notification" do
+    subject(:removal_email) { described_class.user_membership_destroyed_notification(user, organisation) }
+
+    let(:user) { create(:user) }
+    let(:organisation) { create(:school) }
+
+    it "sends expected message to user" do
+      expect(removal_email.to).to contain_exactly user.email_address
+      expect(removal_email.subject).to eq "[DEVELOPMENT] You have been removed from Find placement schools"
+      expect(removal_email.body.to_s.squish).to eq(<<~EMAIL.squish)
+        Dear #{user.first_name},
+
+        You have been removed from the Find placement schools service for #{organisation.name}.
+
+        # Give feedback or report a problem
+
+        If you have any questions or feedback, please contact the team at [find.placementschools@education.gov.uk](mailto:find.placementschools@education.gov.uk).
+
+        Regards
+
+        Find placement schools team
+      EMAIL
+    end
+
+    context "when HostingEnvironment.env is 'production'" do
+      before do
+        allow(HostingEnvironment).to receive(:env).and_return("production")
+      end
+
+      it "does not prepend the hosting environment to the subject" do
+        expect(removal_email.subject).to eq("You have been removed from Find placement schools")
+      end
+    end
+
+    context "when HostingEnvironment.env is 'staging'" do
+      before do
+        allow(HostingEnvironment).to receive(:env).and_return("staging")
+      end
+
+      it "prepends the hosting environment to the subject" do
+        expect(removal_email.subject).to eq("[STAGING] You have been removed from Find placement schools")
+      end
+    end
+  end
+end
