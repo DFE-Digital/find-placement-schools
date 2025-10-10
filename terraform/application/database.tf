42e388666597802702b609d33d82c8eb9acbf584
Elaiy Ganesan <elaiyganesan@gmail.com>
2025-10-10 10:25:16 +0100

    Prod CICD, High availability, Logit, Maintenance window, postgres monitoring enabled

--- terraform/application/database.tf
+++ terraform/application/database.tf
@@ -9,9 +9,12 @@ module "postgres" {
   config_short                = var.config_short
   cluster_configuration_map   = module.cluster_data.configuration_map
   use_azure                   = var.deploy_azure_backing_services
-  azure_enable_monitoring     = var.enable_monitoring
+  azure_enable_monitoring     = true
   azure_enable_backup_storage = var.enable_postgres_backup_storage
   server_version              = "16"
   azure_extensions            = var.postgres_extensions
+  azure_maintenance_window    = var.azure_maintenance_window
+  azure_enable_high_availability = var.postgres_enable_high_availability
+  azure_sku_name              = var.postgres_flexible_server_sku
 }
 
