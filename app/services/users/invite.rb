29cf1173a2a8984a00ca61b095cbd9365c77999d
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-10-24 12:16:02 +0100

    Add wait_time to invite service

--- app/services/users/invite.rb
+++ app/services/users/invite.rb
@@ -1,13 +1,14 @@
 class Users::Invite < ApplicationService
-  def initialize(user:, organisation:)
+  def initialize(user:, organisation:, wait_time: 0.minutes)
     @user = user
     @organisation = organisation
+    @wait_time = wait_time
   end
 
-  attr_reader :user, :organisation, :service
+  attr_reader :user, :organisation, :wait_time
 
   def call
-    mailer_class.user_membership_created_notification(user, organisation).deliver_later
+    mailer_class.user_membership_created_notification(user, organisation).deliver_later(wait: wait_time)
   end
 
   private
