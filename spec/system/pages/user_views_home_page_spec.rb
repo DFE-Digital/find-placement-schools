8527e1cf3cada94774e90a7235b87f1590fd6fda
Jamie <jamiecleare2525@hotmail.com>
2025-10-27 09:52:08 +0000

    Update start page text

--- spec/system/pages/user_views_home_page_spec.rb
+++ spec/system/pages/user_views_home_page_spec.rb
@@ -14,8 +14,24 @@ RSpec.describe "User views home page", type: :system do
 
   def then_i_see_the_home_page
     expect(page).to have_title("Find placement schools")
-    expect(page).to have_h2("Register placement schools")
-    expect(page).to have_h2("Find placement schools")
+    expect(page).to have_h2("Actions for schools")
+    expect(page).to have_paragraph(
+      "Use this service to register your school’s ability to offer school placements for trainee teachers."
+    )
+    expect(page).to have_paragraph(
+      "This helps teacher training providers know whether to contact you and helps the Department for Education" \
+        " understand school capacity to support teacher training."
+    )
+
+    expect(page).to have_h2("Actions for providers")
+    expect(page).to have_paragraph(
+      "Use this service to find placement schools for your trainee teachers."
+    )
+
+    expect(page).to have_inset_text(
+      "This service is being trialled by the Department for Education with schools and teacher training providers in England."
+    )
+
     expect(page).to have_link("Start now", href: "/sign-in")
   end
 end
