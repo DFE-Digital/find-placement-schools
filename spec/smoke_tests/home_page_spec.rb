1d48098c68150b6ce046e9c4f20518e21fc12c29
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-11-04 11:17:17 +0000

    Fix spec name

--- spec/smoke_tests/home_page_spec.rb
+++ spec/smoke_tests/home_page_spec.rb
@@ -1,7 +1,7 @@
 require "smoke_tests/smoke_test_helper"
 
 RSpec.describe "Home Page", :smoke_test, service: :claims, type: :system do
-  it "User visits the claims homepage" do
+  it "User visits the homepage" do
     given_i_am_on_the_start_page
     i_can_see_the_service_name
   end
