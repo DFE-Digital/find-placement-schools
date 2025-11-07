1dc3f7f4c1ebb3dc7d24023c83df6e80a0d35b98
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-11-07 12:08:59 +0000

    Revert optional academic year

--- spec/models/placement_preference_spec.rb
+++ spec/models/placement_preference_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PlacementPreference, type: :model do
   describe "associations" do
-    it { is_expected.to belong_to(:academic_year).optional }
+    it { is_expected.to belong_to(:academic_year) }
     it { is_expected.to belong_to(:organisation) }
     it { is_expected.to belong_to(:created_by).class_name("User") }
   end
