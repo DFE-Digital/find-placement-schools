eb1b8c03bbe94c2e0e2c1b0721d78328c0a0807d
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-11-07 12:07:40 +0000

    Update start page

--- spec/system/pages/user_views_home_page_spec.rb
+++ spec/system/pages/user_views_home_page_spec.rb
@@ -14,21 +14,32 @@ RSpec.describe "User views home page", type: :system do
 
   def then_i_see_the_home_page
     expect(page).to have_title("Find placement schools")
-    expect(page).to have_h2("Actions for schools")
     expect(page).to have_paragraph(
-      "Use this service to register your school’s ability to offer school placements for trainee teachers."
+      "Use this service if you are a school or teacher training provider in England."
     )
     expect(page).to have_paragraph(
-      "This helps teacher training providers know whether to contact you and helps the Department for Education" \
-        " understand school capacity to support teacher training."
+      "All schools in England should use this service."
     )
 
-    expect(page).to have_h2("Actions for providers")
+    expect(page).to have_h2("Schools")
+
     expect(page).to have_paragraph(
-      "Use this service to find placement schools for your trainee teachers."
+      "Use this service to record your school’s ability to offer school placements for trainee teachers."
+    )
+    expect(page).to have_paragraph(
+      "This helps teacher training providers know whether to contact your school and helps the Department for Education understand school capacity to support teacher training."
     )
 
-    expect(page).to have_inset_text(
+    expect(page).to have_h2("Teacher training providers")
+    expect(page).to have_paragraph(
+      "Use this service to find placement schools your trainee teachers. You can view information about the schools and find contact details to enquire about placements."
+    )
+
+    expect(page).to have_h2("Before you get started")
+    expect(page).to have_paragraph(
+      "To access this service, you will be asked to sign in or create a Department for Education (DfE) Sign-in account. DfE Sign-in is how schools and other education organisations access DfE online services."
+    )
+    expect(page).to have_paragraph(
       "This service is being trialled by the Department for Education with schools and teacher training providers in England."
     )
 
