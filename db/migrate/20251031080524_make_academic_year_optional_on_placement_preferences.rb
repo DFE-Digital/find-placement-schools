f3d492a62ee02412bdf03a91ba0c70ed4c8278d0
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-11-07 12:08:59 +0000

    Revert optional academic year

--- db/migrate/20251031080524_make_academic_year_optional_on_placement_preferences.rb
+++
@@ -1,6 +0,0 @@
-class MakeAcademicYearOptionalOnPlacementPreferences < ActiveRecord::Migration[7.0]
-  def change
-    # allow NULLs for the FK column
-    change_column_null :placement_preferences, :academic_year_id, true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_31_080524) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_28_095558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -97,7 +97,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_31_080524) do
   end
 
   create_table "placement_preferences", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "academic_year_id"
+    t.uuid "academic_year_id", null: false
     t.uuid "organisation_id", null: false
     t.uuid "created_by_id", null: false
     t.enum "appetite", enum_type: "appetite"
