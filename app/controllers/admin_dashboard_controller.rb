3e7c187474b01f975204633f38f391c06c5b3f3d
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-11-10 11:48:12 +0000

    Deny access to non-admins and remove job dashboard password

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -1,3 +1,14 @@
 class AdminDashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin
+
   def index; end
+
+  private
+
+  def require_admin
+    unless current_user&.admin?
+      head :forbidden
+    end
+  end
 end
