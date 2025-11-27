0329f202fc031e41e1236cc4f3e314978eedc111
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2025-11-27 10:28:19 +0000

    Add methods to tidy up service navigation

--- app/components/service_navigation_component.rb
+++ app/components/service_navigation_component.rb
@@ -10,14 +10,14 @@ class ServiceNavigationComponent < ApplicationComponent
         {
           text: t("components.service_navigation_component.dashboard"),
           href: helpers.admin_dashboard_index_path,
-          active: request.path.match?(/^\/admin_dashboard/),
-          current: request.path.match?(/^\/admin_dashboard/)
+          active: matches_admin_dashboard_path?,
+          current: matches_admin_dashboard_path?
         },
         {
           text: t("components.service_navigation_component.users"),
           href: helpers.admin_users_path,
-          active: request.path.match?(/^\/admin\/users/),
-          current: request.path.match?(/^\/admin\/users/)
+          active: matches_admin_users_path?,
+          current: matches_admin_users_path?
         }
       ]
     elsif current_user && current_organisation
@@ -27,20 +27,20 @@ class ServiceNavigationComponent < ApplicationComponent
           {
             text: t("components.service_navigation_component.placement_information"),
             href: helpers.placement_preferences_path,
-            active: request.path.match?(/^\/placement_preferences/),
-            current: request.path.match?(/^\/placement_preferences/)
+            active: matches_placement_information_path?,
+            current: matches_placement_information_path?
           },
           {
             text: t("components.service_navigation_component.users"),
             href: helpers.users_path,
-            active: request.path.match?(/^\/users/),
-            current: request.path.match?(/^\/users/)
+            active: matches_users_path?,
+            current: matches_users_path?
           },
           {
             text: t("components.service_navigation_component.organisation_details"),
             href: helpers.organisation_path(current_organisation.id),
-            active: helpers.current_page?(helpers.organisation_path(current_organisation.id)),
-            current: helpers.current_page?(helpers.organisation_path(current_organisation.id))
+            active: matches_organisation_details_path?,
+            current: matches_organisation_details_path?
           }
         ]
       when Provider
@@ -48,20 +48,20 @@ class ServiceNavigationComponent < ApplicationComponent
           {
             text: t("components.service_navigation_component.find_placements"),
             href: helpers.organisations_path,
-            active: request.path.match?(/^\/organisation/) && !helpers.current_page?(helpers.organisation_path(current_organisation.id)),
-            current: request.path.match?(/^\/organisation/) && !helpers.current_page?(helpers.organisation_path(current_organisation.id))
+            active: matches_find_placements_path?,
+            current: matches_find_placements_path?
           },
           {
             text: t("components.service_navigation_component.users"),
             href: helpers.users_path,
-            active: request.path.match?(/^\/users/),
-            current: request.path.match?(/^\/users/)
+            active: matches_users_path?,
+            current: matches_users_path?
           },
           {
             text: t("components.service_navigation_component.organisation_details"),
             href: helpers.organisation_path(current_organisation.id),
-            active: helpers.current_page?(helpers.organisation_path(current_organisation.id)),
-            current: helpers.current_page?(helpers.organisation_path(current_organisation.id))
+            active: matches_organisation_details_path?,
+            current: matches_organisation_details_path?
           }
         ]
       else
@@ -79,4 +79,28 @@ class ServiceNavigationComponent < ApplicationComponent
   private
 
   attr_reader :current_user, :current_organisation
+
+  def matches_find_placements_path?
+    request.path.match?(/^\/organisation/) && !helpers.current_page?(helpers.organisation_path(current_organisation.id))
+  end
+
+  def matches_users_path?
+    request.path.match?(/^\/users/)
+  end
+
+  def matches_organisation_details_path?
+    helpers.current_page?(helpers.organisation_path(current_organisation.id))
+  end
+
+  def matches_placement_information_path?
+    request.path.match?(/^\/placement_preferences/)
+  end
+
+  def matches_admin_dashboard_path?
+    request.path.match?(/^\/admin_dashboard/)
+  end
+
+  def matches_admin_users_path?
+    request.path.match?(/^\/admin\/users/)
+  end
 end
