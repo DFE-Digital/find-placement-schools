83ed64047bb57470d87be78fe5e30274755c46d9
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-02-10 16:40:24 +0000

    Update missed call

--- spec/mailers/previews/schools/user_mailer_preview.rb
+++ spec/mailers/previews/schools/user_mailer_preview.rb
@@ -1,6 +1,6 @@
 class Schools::UserMailerPreview < ActionMailer::Preview
   def user_membership_sign_in_reminder_notification
-    School::UserMailer.user_membership_sign_in_reminder_notification(user)
+    School::UserMailer.user_membership_sign_in_reminder_notification(user, organisation)
   end
 
   def placement_preferences_reminder_notification
@@ -15,5 +15,9 @@ class Schools::UserMailerPreview < ActionMailer::Preview
 
   def user
     User.new(first_name: "Joe", last_name: "Bloggs", email_address: "joe_bloggs@example.com")
+  end
+
+  def organisation
+    Organisation.new(name: "Hogwarts School of Witchcraft and Wizardry", urn: "123456", type: "School")
   end
 end
