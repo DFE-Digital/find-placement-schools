c6a3333bdc36f9389a7a294a2b41f89e34d98094
Ben Abraham <16797406+Kizr@users.noreply.github.com>
2026-07-03 15:40:51 +0100

    Remove second footer and make new footer sticky to flow more naturally on small pages

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /" do
-    it "returns http success" do
-      get "/"
+  shared_examples "a page with a single semantic footer" do |path|
+    it "renders layout and footer correctly for #{path}" do
+      get path
+
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('class="govuk-template__body app-page"')
+      expect(response.body).to include('class="govuk-main-wrapper app-page__main"')
+      expect(response.body.scan(/<footer\b/).size).to eq(1)
+      expect(response.body.scan(/<footer[^>]*class="[^"]*\bgovuk-footer\b[^"]*"/).size).to eq(1)
+      expect(response.body).not_to include('<div class="govuk-footer"')
     end
   end
+
+  include_examples "a page with a single semantic footer", "/"
+  include_examples "a page with a single semantic footer", "/cookies"
+  include_examples "a page with a single semantic footer", "/privacy"
+  include_examples "a page with a single semantic footer", "/accessibility"
+  include_examples "a page with a single semantic footer", "/terms_and_conditions"
 end
